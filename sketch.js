0f208c30ff701b9b629d0ade579ee255a296dbb7
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-23 17:57:41 +0100

    added reset population logic

--- sketch.js
+++ sketch.js
@@ -7,12 +7,19 @@ function setup() {
   createCanvas(400, 300);
   population = new Population();
   lifeP = createP(count);
+  target = createVector(width/2, 50);
 }
 
 function draw() {
   background(0);
   population.run();
-
   lifeP.html(count);
   count++;
+
+  if (count == lifespan) {
+    population = new Population();
+    count = 0;
+  }
+
+  ellipse(target.x, target.y, 16, 16)
 }
