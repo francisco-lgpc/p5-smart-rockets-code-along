3adddd454affc08a5f049aa7e338d92c9023c3a1
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-23 18:58:33 +0100

    updated fitness function

--- rocket.js
+++ rocket.js
@@ -4,20 +4,28 @@ function Rocket(dna) {
 	this.acc = createVector();
 	this.dna = dna ? dna : new DNA();
 	this.fitness = 0;
+	this.hitTarget = false;
 
 	this.applyForce = function(force) {
 		this.acc.add(force);
 	}
 
 	this.update = function() {
-		this.applyForce(this.dna.genes[count]);
+		let d = dist(this.pos.x, this.pos.y, target.x, target.y)
+		if (d < 10) {
+			this.hitTarget = true;
+			this.pos = target.copy();
+		}
 
-		this.vel.add(this.acc);
-		this.pos.add(this.vel);
-		this.acc.mult(0);	
+		this.applyForce(this.dna.genes[count]);
+		if (!this.hitTarget) {
+			this.vel.add(this.acc);
+			this.pos.add(this.vel);
+			this.acc.mult(0);	
+		}
 	}
 
-	this.show =function() {
+	this.show = function() {
 		push();
 		noStroke();
 		fill(255, 150);
@@ -31,5 +39,8 @@ function Rocket(dna) {
 	this.calcFitness = function() {
 		let d = dist(this.pos.x, this.pos.y, target.x, target.y);
 		this.fitness = map(d, 0, width, width, 0);
+		if (this.hitTarget) {
+			this.fitness *= 10;
+		}
 	}
 }
